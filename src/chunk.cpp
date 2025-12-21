74cafc4dff7ab63659c87dbcb4d8c8b109809b84
Penelope Yong <penelopeysm@gmail.com>
2025-12-21 01:57:33 +0000

    improved makefile

--- src/chunk.cpp
+++ src/chunk.cpp
@@ -5,6 +5,51 @@
 
 Chunk::Chunk() : code() {}
 
+// NOTE: Sometimes, tiny functions like these are defined inside the header.
+// There are a few reasons why one might do that:
+//   1. Inlining: Because header files are included directly in source files,
+//      the compiler can see the full definition of Chunk::size(), and can
+//      potentially inline the definition, leading to improved performance.
+//      This is not possible if Chunk::size() is defined in a separate source
+//      file, because each cpp file is its own compilation unit (the idea being
+//      that if you change a definition in a cpp file, you shouldn't have to
+//      recompile every other cpp file -- whereas if you change a header file,
+//      you have to recompile every cpp file that includes it).
+//      (In fact, some compilers can do "link-time optimisation" which inlines
+//      functions across compilation units. But we won't worry about that.)
+//   2. Template classes. I don't understand this fully yet.
+//   3. "Header-only libraries": some libraries are designed to be used by only
+//      importing a header, instead of having cpp files which must be compiled
+//      and linked against. If that is the intent, then all definitions must be
+//      contained in the header files.
+size_t Chunk::size() const {
+  return code.size();
+}
+
+size_t Chunk::capacity() const {
+  return code.capacity();
+}
+
+Chunk &Chunk::write(uint8_t byte) {
+  try {
+    // NOTE: try/catch are (almost?) zero-cost in the non-exception path
+    code.push_back(byte);
+  } catch (const std::bad_alloc &) {
+    // Gracefully handle OOM.
+    // NOTE: std::vector will clean up its own memory if an exception is
+    // thrown, we don't need to do anything special to avoid leaks!
+    throw std::runtime_error("loxc: Out of memory while writing to Chunk");
+  }
+  return *this;
+}
+
+Chunk &Chunk::reset() {
+  // NOTE: clear() removes elements and so size() will return 0, but does not
+  // change capacity
+  code.clear();
+  return *this;
+}
+
 std::ostream &operator<<(std::ostream &os, const Chunk &chunk) {
   // NOTE: "\n" vs std::endl: the latter always flushes the stream, which is
   // not always what we want (e.g. if writing to a file, it may be more
@@ -27,15 +72,3 @@ std::ostream &operator<<(std::ostream &os, const Chunk &chunk) {
   return os;
 }
 
-Chunk &Chunk::write(uint8_t byte) {
-  try {
-    // NOTE: try/catch are (almost?) zero-cost in the non-exception path
-    code.push_back(byte);
-  } catch (const std::bad_alloc &) {
-    // Gracefully handle OOM.
-    // NOTE: std::vector will clean up its own memory if an exception is
-    // thrown, we don't need to do anything special to avoid leaks!
-    throw std::runtime_error("loxc: Out of memory while writing to Chunk");
-  }
-  return *this;
-}
--- src/chunk.hpp
+++ src/chunk.hpp
@@ -17,9 +17,20 @@ class Chunk {
 public:
   Chunk();
 
+  size_t size() const;
+  size_t capacity() const;
   Chunk &write(uint8_t byte);
+  Chunk &reset();
 
-  // NOTE: `friend` allows this function to access private members of Chunk
-  // (such as `code`) which is necessary for printing.
+  // NOTE: `friend` allows the implementation of this function to access
+  // private members of Chunk (such as `code`) which is necessary for printing.
+  // Note that member functions (like `write` above) don't have such
+  // restrictions: they can always access private members of their own class.
+  // However, member functions also get an implicit `this` parameter as the
+  // first argument, whereas friend functions do not. We COULD make operator<<
+  // a member function, something like
+  //   std::ostream &operator<<(std::ostream &os) const;
+  // but that would make its usage more awkward, since we would have to write
+  // `chunk << std::cout` instead of the more natural `std::cout << chunk`.
   friend std::ostream &operator<<(std::ostream &os, const Chunk &chunk);
 };
