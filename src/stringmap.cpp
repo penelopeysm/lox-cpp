217db9e516342e4822794d61f75fb9ce43edb77b
Penelope Yong <penelopeysm@gmail.com>
2026-01-03 18:56:45 +0000

    print

--- src/stringmap.cpp
+++ src/stringmap.cpp
@@ -9,10 +9,19 @@ namespace lox {
 std::shared_ptr<ObjString> StringMap::get_ptr(std::string_view key) {
   auto it = map.find(key);
   if (it == map.end()) {
+#ifdef LOX_DEBUG
+    std::cout << "StringMap: Creating new ObjString for key: " << key
+              << std::endl;
+#endif
     // Not found; create a new one.
     std::shared_ptr<ObjString> ptr = std::make_shared<ObjString>(key);
     auto new_pair = map.emplace(std::string(key), ptr);
     it = new_pair.first;
+  } else {
+#ifdef LOX_DEBUG
+    std::cout << "StringMap: Found existing ObjString for key: " << key
+              << std::endl;
+#endif
   }
   return it->second;
 }
